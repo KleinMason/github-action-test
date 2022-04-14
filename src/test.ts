5efc5f21394d9fe2fef9ecada539651b85b65737
Mason Kleinsorge <Kleinsorgem97@gmail.com>
2022-04-14 16:41:00 -0500

    remove test.ts and test.spec.ts

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-export class TestClass {
-    get prompt(): string { return "Hello World!" }
-
-    printPrompt = (): Promise<void> => {
-        console.log(this.prompt);
-        return Promise.resolve();
-    }
-}
-
